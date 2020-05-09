9d112e27972cb1ff975b076404b8e78ba59963c7
BattySingh <singhbatty02@gmail.com>
2020-05-09 19:01:15 +0530

    Started learning about javascript

---
+++ coding/javascript/add.js
@@ -0,0 +1,12 @@
+var sum = 32 + 3;
+console.log(sum);
+
+// incrementing a number
+sum++;
+console.log(sum);
+
+var myDecimal = 3.47;
+console.log(myDecimal);
+
+sum+=5; // compound statement
+console.log(sum);
---
+++ coding/javascript/alert.js
@@ -0,0 +1,3 @@
+window.alert('This is an alert statement');
+alert('Learning Javascript');
+console.log('Jurassic World');
